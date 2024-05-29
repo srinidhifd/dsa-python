dba77119ae8a34040ca3910741ba0a5b328a770f
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-05-30 01:34:43 +0530

    Added operations.py file

--- operations.py
+++ operations.py
@@ -35,3 +35,38 @@ print(lst)
 
 
 # tuple is immutable , we can just access the values
+
+# dict - {key:value}
+
+third_sem_marks={"Python":96,"IOT":82,"NOSQL":86,"C#":82}
+
+# adding values
+third_sem_marks["IOT-Lab"]=99
+print(third_sem_marks)
+
+# accessing values using keys
+print(third_sem_marks["Python"])
+
+
+# to remove value
+del third_sem_marks["IOT-Lab"]
+print(third_sem_marks)
+
+
+# get() it wont return error if that key not present
+
+print(third_sem_marks.get("IOT1",-1))
+
+
+# set {} a = set() , unique values, unordered - very efficient faster access
+
+n={1,2,3,4,5}
+
+# add()
+n.add(20)
+print(n)
+
+
+# remove
+n.remove(20)
+print(n)
