498dd0a9c8cb43fcd700334aed62e87d99d3f733
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-05-29 23:54:05 +0530

    Added output-ways.py file

---
+++ output-ways.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+x,y=2,3
+print("Sum=",x+y)
+
+#by default print() has "\n" at end
+name="Srinidhi"
+age="22"
+print(name)
+print(age)
+print(name,end=" ")
+print(age)
+
+# here it will end with - symbol
+name="Srinidhi"
+age="22"
+print(name,end="-")
+print(age)
