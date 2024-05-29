3408a74ec859585d51753f7fb6ee849cdf8f06b0
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-05-30 00:40:38 +0530

    Added datatypes.py file

--- datatypes.py
+++ datatypes.py
@@ -2,3 +2,18 @@
 # it tells the type of data in variable 
 
 # str/int/float/bool/set {}/list []/tuple ()/dict {}/None are datatypes in python
+
+# Example
+
+name="Srinidhi"
+age=22
+isGraduate=False
+CGPA=9.11
+
+hobbies=["chess","music","football"]
+
+no_of_sem=("first","second","third","fourth")
+
+skills={"HTML","CSS","Bootstrap","Javascript","Angular","Python","PHP","Mysql"}
+
+third_sem_marks={"Python":96,"IOT":82,"NOSQL":86,"C#":82}
