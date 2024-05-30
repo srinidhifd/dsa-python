3db8c3bf6827c9f8ee8f439a844448b8465a486b
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-05-30 15:16:12 +0530

    Added time-space-complexity.py file

--- time-space-complexity.py
+++ time-space-complexity.py
@@ -1,1 +1,14 @@
 # time and space complexity 
+
+# time complexity - how much time taken to execute as input increases , not dependednt on execution time on machine 
+
+# best case - Omega notation  O(1)
+# worst case - Big O notation O(n)
+# average case - Theta notation 
+
+# for i in range(n):
+#   for j in range(n): 
+#complexity will be n*n = n2
+
+
+# space complexity - how much memory is required to execute the code
