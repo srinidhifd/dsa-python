2c8ef90f676cda706b28e933b94eb3249f585a8b
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-05-30 12:14:11 +0530

    Added coercion.py file

---
+++ coercion.py
@@ -0,0 +1,17 @@
+# type conversion - one datatype to another
+
+int()
+str()
+float()
+bool()
+list()
+tuple()
+set()
+
+# zip(list1,list2) is used to combine to lists into key value pair
+res = dict(zip(lst1,lst2))
+
+
+# res = (5) is integer not tuple , whereas (5,) is a tuple
+
+# False values - empty [] , () , {} , "" , 0 , 0.0
