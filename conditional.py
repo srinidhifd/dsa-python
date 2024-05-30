e6ad7b2349c43b5d4907cfb523d3159ed77889c7
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-05-30 12:28:21 +0530

    Added conditional.py file

---
+++ conditional.py
@@ -0,0 +1,15 @@
+# conditional statements 
+
+# if 
+# if else
+# if elif
+# if elif elif 
+# if elif elif elif
+# if elif elif elif else  
+
+if(age>18):
+    print("can vote")
+else:
+    ("cannot vote")
+
+# no switch case in python
