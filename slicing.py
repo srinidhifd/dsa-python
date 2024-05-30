db410557f61aa0296e3a7e941991daba72d4f69c
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-05-30 12:25:03 +0530

    Added slicing.py file

---
+++ slicing.py
@@ -0,0 +1,15 @@
+# slicing 
+
+# str[start:end:steps]
+
+# str[::-1] - to reverse a string
+
+# str[::] whole string 
+
+# always starting is 0 or first character by default
+
+# ending by default is last character in string 
+
+# string to list can be done by using split()
+
+# list to string can be done using join()
