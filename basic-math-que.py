4c02d5ca47a66c968fb99dc5c541dc1b65a0494e
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-05-31 18:35:46 +0530

    Added basic-math-que.py file

--- basic-math-que.py
+++ basic-math-que.py
@@ -3,7 +3,12 @@
 # count digits in the number 
 
 def countNumber(num):
-    return len(str(num))
+    count=0
+    while num>0 :
+        num=num//10
+        count=count+1
+    return count
+    # return len(str(num))
 
 num=int(input())
 res=countNumber(num)
